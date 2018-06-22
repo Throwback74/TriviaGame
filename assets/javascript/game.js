036ece1c333791af2970b35625b569c362d23d1d
Throwback74 <corey.slade@gmail.com>
2018-06-21 17:07:52 -0700

    leaving class for day Thursday

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,85 +4,113 @@
         var myArrayIndex = 0;
 
         // var Qs = 0; Qs < arrayLength; Qs++;
-        var myArray = [{
+        var myArray = {
+            
+            uselessTrivia: [
+            {
+                q: "1. How do crickets hear?",
+                
+                
+            },
+            ]
+            trueFalse: [
+            {    
                 q: "Is the sky blue?",
-                a: "t"
+                a: "t",
             },
             {
                 q: "Is Gandalf an Elf?",
-                a: "f"
+                a: "f",
             },
             {
                 q: "An ostrich&#39;s eye is bigger than its brain.",
-                a: "t"
+                a: "t",
             },
             {
                 q: "The phrase &#39;O.M.G.&#39; was used in 1917 in a letter to Winston Churchill.",
-                a: "t"
+                a: "t",
             },
             {
                 q: "A duck&#39;s quack doesn&#39;t echo.",
-                a: "f"
+                a: "f",
             },
             {
                 q: "When David Hasselhoff divorced, he lost possession of the catchphrase &#34;Don&#39;t Hassle the Hoff.&#34;",
-                a: "f"
+                a: "f",
             },
             {
                 q: "Bubble gum contains rubber.",
-                a: "t"
+                a: "t",
             },
             {
                 q: "Camels have three sets of eyelids.",
-                a: "t"
+                a: "t",
             },
             {
                 q: "A jellyfish is 95 percent water.",
-                a: "t"
+                a: "t",
             },
             {
                 q: "Los Angeles is farther west than Reno.",
-                a: "f"
+                a: "f",
             },
-        ]
-
-        function renderQuestion() {
-            if (questionIndex <= (questions.length - 1)) {
-            document.querySelector("#question").innerHTML = questions[questionIndex].q;
-            }
-            else {
-            document.querySelector("#question").innerHTML = "Game Over!";
-            document.querySelector("#score").innerHTML = "Final Score: " + wins + " out of " + myArray.length;
-            }
+            ]
         }
 
-        $("")
 
 
 
-        document.onkeyup = function (event) {
+//    <form id="Q1">
+{/* <fieldset>
+<label class="radio-inline">
+    <input type="radio" name="gender" value="male" checked> Male<br>
+</label>
+<label class="radio-inline">
+    <input type="radio" name="gender" value="female"> Female<br>
+</label>
+<label class="radio-inline">
+    <input type="radio" name="gender" value="other"> Other
+</label>
+</fieldset>
+</form> */}
 
-            if (myArrayIndex === myArray.length) {
-                return;
-            }
+        // function renderQuestion() {
+        //     if (questionIndex <= (questions.length - 1)) {
+        //     document.querySelector("#question").innerHTML = questions[questionIndex].q;
+        //     }
+        //     else {
+        //     document.querySelector("#question").innerHTML = "Game Over!";
+        //     document.querySelector("#score").innerHTML = "Final Score: " + wins + " out of " + myArray.length;
+        //     }
+        // }
 
-            var userGuess = event.key;
-
-
-
-            if ((userGuess === "t") || (userGuess === "f")) {
-
-                if (userGuess === myArray[myArrayIndex].a) {
-                    alert("Correct!");
-                    wins++;
-
-                } else {
-                    alert("Wrong! ")
-                    losses--;
-                }
-                myArrayIndex++;
-                document.write(myArray[myArrayIndex].q);
+        // $("")
 
 
-            }
-        }
+
+        // document.onkeyup = function (event) {
+
+        //     if (myArrayIndex === myArray.length) {
+        //         return;
+        //     }
+
+        //     var userGuess = event.key;
+
+
+
+        //     if ((userGuess === "t") || (userGuess === "f")) {
+
+        //         if (userGuess === myArray[myArrayIndex].a) {
+        //             alert("Correct!");
+        //             wins++;
+
+        //         } else {
+        //             alert("Wrong! ")
+        //             losses--;
+        //         }
+        //         myArrayIndex++;
+        //         document.write(myArray[myArrayIndex].q);
+
+
+            // }
+        // }
